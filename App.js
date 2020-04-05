62a5cbf6e27572033a86a1044d0be0478f36bb23
varunkverma <varunkverma1996@gmail.com>
2020-04-05 13:56:34 +0530

    added title, input field and buttons on the start game screen with some styling

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
+import StartGameScreen from "./screens/start-game-screen";
 import Header from "./components/header.component";
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess A Number" />
+      <StartGameScreen />
     </View>
   );
 }
---
+++ screens/start-game-screen.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.Title}>Start A New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <Button title="Reset" onPress={() => {}} />
+          <Button title="Confirm" onPress={() => {}} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: "80%",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+  },
+});
+
+export default StartGameScreen;
