53ce41600a06631dbea4f0219472f8f5624d338f
varunkverma <varunkverma1996@gmail.com>
2020-04-05 14:34:21 +0530

    added a card component to create a card like containers, it accepts width, maxWidth and alignItems values as prop

--- screens/start-game-screen.jsx
+++ screens/start-game-screen.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
+import Card from "../components/card.component";
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start A New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={{ ...styles.inputContainer }}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" onPress={() => {}} />
           <Button title="Confirm" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -31,17 +32,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      height: 2,
-      width: 0,
-    },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    elevation: 5,
-    backgroundColor: "white",
-    padding: 20,
-    borderRadius: 10,
   },
   buttonContainer: {
     flexDirection: "row",
